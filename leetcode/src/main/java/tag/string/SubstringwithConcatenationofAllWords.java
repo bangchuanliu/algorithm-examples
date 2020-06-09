4be629f5abc13630d13faca2bc7675dd13c237a1
liubangchuan <liubangchuan@liubanghuansMBP.attlocal.net>
2020-06-08 23:27:27 -0700

    new solution

--- leetcode/src/main/java/tag/string/SubstringwithConcatenationofAllWords.java
+++ leetcode/src/main/java/tag/string/SubstringwithConcatenationofAllWords.java
@@ -44,9 +44,40 @@ public class SubstringwithConcatenationofAllWords {
         return result;
     }
 
+    public List<Integer> findSubstring(String s, String[] words, int index) {
+        if (s == null || s.length() == 0 || words == null || words.length == 0 || s.length() < words.length * words[0].length()) {
+            return new ArrayList<>();
+        }
+
+        int len = words[0].length();
+        List<Integer> result = findSubstring(s.substring(1), words, index + 1);
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        int i = 0;
+        int j = 0;
+
+        while (i + len <= s.length() && j < words.length && map.containsKey(s.substring(i, i + len))) {
+            String str = s.substring(i, i + len);
+            map.put(str, map.get(str) - 1);
+            if (map.get(str) == 0) {
+                map.remove(str);
+            }
+            i = i + len;
+            j++;
+        }
+        if (map.size() == 0) {
+            result.add(index);
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         SubstringwithConcatenationofAllWords substringwithConcatenationofAllWords = new SubstringwithConcatenationofAllWords();
-        String s = "wordgoodgoodgoodbestword";
+        String s = "wordwordword";
         System.out.println(s.length());
         String[] words = {"word", "good", "best", "good"};
         System.out.println(Arrays.toString(substringwithConcatenationofAllWords.findSubstring(s, words).toArray()));
